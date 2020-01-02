abe35aa8c97bd3e5b1e2b4ac224464aefa772c43
abanoubmesiha <53008015+abanoubmesiha@users.noreply.github.com>
2020-01-02 19:09:58 +0200

    toggle sidebar

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() Toggler: EventEmitter<void> = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit() {
   }
-
+  toggleSidebar(){
+    this.Toggler.emit();
+  }
 }
